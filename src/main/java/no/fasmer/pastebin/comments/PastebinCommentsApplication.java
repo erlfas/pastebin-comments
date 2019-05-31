04494eea06ecedf07a58fd20127ac9559f768390
erlfas <erlfas@users.noreply.github.com>
2019-05-31 18:22:30 +0200

    make compilable

--- src/main/java/no/fasmer/pastebin/comments/PastebinCommentsApplication.java
+++ src/main/java/no/fasmer/pastebin/comments/PastebinCommentsApplication.java
@@ -1,6 +1,13 @@
 package no.fasmer.pastebin.comments;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
+@SpringCloudApplication
 public class PastebinCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinCommentsApplication.class);
+    }
     
 }
